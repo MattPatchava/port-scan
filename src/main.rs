80245366f91d6ed3557284c92957b506dc300988
Matt Patchava <mattpatchava@outlook.com>
2025-07-31 17:52:02 +1000

    feat: add CLI parsing with positional args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "port-scan", version, about, long_about = None)]
+struct Args {
+    /// Target host to scan
+    target: String,
+
+    /// Start port
+    start_port: u16,
+
+    /// Optional end port
+    end_port: Option<u16>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Args::parse();
+
+    // TODO: Parse target into IP address struct and use this for later access (rather than the
+    // args struct)
+
+    let start_port: u16 = args.start_port;
+    let end_port: u16 = match args.end_port {
+        Some(i) => i,
+        None => start_port,
+    };
+
+    println!(
+        "IP to scan: {}, port(s) to scan: {} to {} (inclusive)",
+        args.target, start_port, end_port
+    );
 }
